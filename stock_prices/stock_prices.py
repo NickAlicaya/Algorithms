edc24091c2f605a0056d053f32d3c7124668f9d9
nicanor-alicaya <donalicaya@gmail.com>
2020-04-15 19:03:59 -0400

    stockprices passing test, cleaned up

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python
 
 import argparse
-# prices=[800,300,32,500,750,600]
 def find_max_profit(prices):
   highest_value=max(prices[1:])
-  # print(highest_value)
   tip=prices.index(highest_value)
-  # print(tip)
   lowest_range=prices[:tip]
-  # print(lowest_range)
   lowest_value=min(lowest_range)
-  # print(lowest_value)
   profit=highest_value-lowest_value
   return profit
-# print(find_max_profit(prices))
-  
-
-
+ 
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
